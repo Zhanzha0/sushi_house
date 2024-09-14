74fa743e7223c2696e8d9ea766267e809aafec87
Zhanzha0 <two00twokt@gmail.com>
2024-09-14 12:23:47 +0630

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'logo' : '#F62500',
+        'cmt' : '#7E322517',
+        'btn' : '#FF6347'
+      }
+    },
+  },
+  plugins: [],
+}
+
